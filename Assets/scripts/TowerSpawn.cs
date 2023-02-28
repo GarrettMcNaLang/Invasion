e3bd846527802c240c61c4f5e19aee31c0c49a26
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2023-02-27 23:52:31 -0500

    assets/tilemap/workingonspawner

---
+++ Assets/scripts/TowerSpawn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawn : MonoBehaviour
+{
+    //list of tower prefabs that will instantiate
+
+    public List<GameObject> tower;
+    //id of tower to spawn
+
+    int spawnID = -1;
+
+}
